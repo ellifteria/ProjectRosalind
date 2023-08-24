10844485edb8bb659b39b23bbe380d0ea7730561
Elli Beres <elliberes@proton.me>
2023-08-24 08:41:12 -0500

    Elixir solution to PERM

---
+++ perm/elixir/perm.exs
@@ -0,0 +1,31 @@
+defmodule PERM do
+  def generate_permutations(list) do
+    case list do
+      [] -> [[]]
+      _ -> for x <- list, y <- generate_permutations(list -- [x]), do: [x|y]
+    end
+  end
+
+  def generate_string(list) do
+    list |>
+    Enum.reduce("", fn(x, acc) -> acc <> String.trim(Enum.reduce(x, fn(x2, acc2) -> to_string(acc2) <> " " <> to_string(x2) end)) <> "\n" end) |>
+    String.trim()
+  end
+end
+
+##########
+#  MAIN  #
+##########
+
+{n, _} = IO.gets("") |> String.trim() |> Integer.parse()
+
+IO.puts("Analyzing:\n" <> to_string(n))
+
+permutations = PERM.generate_permutations(
+  Enum.reduce(1..n, [], &([&1 | &2])) |> Enum.reverse()
+)
+
+IO.puts("\nResult:\n" <>
+to_string(permutations |> length()) <>
+"\n" <>
+PERM.generate_string(permutations))
